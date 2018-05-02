24711c55651a2405355489f7bc343014e3ec5acd
rahatsizarmy <37729661+rahatsizarmy@users.noreply.github.com>
2018-05-02 15:54:56 +0300

    Add files via upload

---
+++ events/guildMemberAdd.js
@@ -0,0 +1,9 @@
+module.exports = member => {
+    let modlog = ('name', 'giris-cıkıs');
+    let username = member.user.username;
+    const embed = new Discord.RichEmbed()
+    .setColor(0x00AE86)
+    .setTimestamp()
+    .addField('sa')
+  return (modlog.id).sendEmbed(embed);
+};
---
+++ events/ready.js
@@ -0,0 +1,14 @@
+const chalk = require('chalk');
+const moment = require('moment');
+const Discord = require('discord.js');
+const ayarlar = require('../ayarlar.json');
+
+var prefix = ayarlar.prefix;
+
+module.exports = client => {
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Tüm komutlar başarıyla yüklendi StormBot`);
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: ${client.user.username} ismi ile giriş yapıldı!`);
+  client.user.setPresence ({ game: { name: `${prefix}yardım | ${client.guilds.size} sunucu | V.2.0.1`, url: 'https://www.twitch.tv/batumurt1', type: 0 } });
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Oyun ismi ayarlandı!`);
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Şu an ` + client.channels.size + ` adet kanala, ` + client.guilds.memberCount + ` adet sunucuya ve ` + client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString() + ` kullanıcıya hizmet veriliyor!`);
+};
